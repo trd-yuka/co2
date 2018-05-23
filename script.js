4251ccbd14b1771fd1e7546d69d64e2241741bb5
Yuka yashiro <trd.yuka@gmail.com>
2018-05-23 11:15:42 +0800

    update menu style.

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
+	//for toogle dropdown
+	$(".dropdown-toggle").click(function(){
+		$(".dropdown-menu").toggle();
+	});
+	
+	//for scroll animation
+	var scrollLink = $('.scroll');
+	scrollLink.click(function(e) { 
+		e.preventDefault();
+		var off = (this.hash ==　"#CONCEPT") ? 100 : 70 
+		
+	    $('body, html').animate({
+	        scrollTop: $(this.hash).offset().top - off
+	    }, 1000,"easeInOutExpo");
+		
+		$("#collapsibledropdown").hide();
+	});
 
-var scrollLink = $('.scroll');
-
-scrollLink.click(function(e) { 
-	e.preventDefault();
-	var off = (this.hash == "#CONCEPT") ? 100 : 70;
-    $('body, html').animate({
-        scrollTop: $(this.hash).offset().top - off
-    }, 1000,"easeInOutExpo");
-    
-    $("#collapsibleNavbar").removeClass("show");
-    
-});
- 
 });
